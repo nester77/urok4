2fd301f8ed2e36a1d88c32708159a6e2caba158f
admin <alex.nester2477@gmail.com>
2020-10-20 20:12:17 +0300

    2 редакция

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Rectangle rect1=new Rectangle (7);
+        Rectangle rect2=new Rectangle (5,3);
+        Rectangle rect3=new Rectangle (8,2);
+
+        rect1.printArea();
+        rect2.printArea();
+        rect3.printArea();
+
+        rect1.printRectangleKind();
+        rect2.printRectangleKind();
+        rect3.printRectangleKind();
+
+        System.out.print(rect1.isTheSameRectangle(rect2)) ;
     }
 }
-//
-//
-//
-//
-//
-//
-//
---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Rectangle {
+
+    double x;
+    double y;
+
+    public Rectangle (double a, double b) {
+        x=a;
+        y=b;
+    }
+
+    public Rectangle (double a) {
+        x=a;
+        y=a;
+    }
+
+    double calculateArea (){
+        double s=x*y;
+        return s;
+    }
+    void printArea(){
+        System.out.println(calculateArea());
+    }
+    void printRectangleKind(){
+        if (x==y){
+
+            System.out.println("Это квадрат");}
+        else
+            System.out.println("Это прямоугольник");
+    }
+    boolean isTheSameRectangle (Rectangle rectangle ){
+        return (this.x==rectangle.x && this.y==rectangle.y) || ((this.x==rectangle.y && this.y==rectangle.x));
+
+    }
+}
